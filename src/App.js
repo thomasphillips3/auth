f8aff0f493eeee252d4d604598e70e336ab475ab
Thomas Phillips <thomasphillips3@gmail.com>
2018-05-30 19:35:19 -0700

    Render content based on logged in state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCmmXJKmMqUbQinqE1owIgyCMYVayEUjYI',
@@ -14,13 +15,46 @@ class App extends Component {
       storageBucket: 'auth-73677.appspot.com',
       messagingSenderId: '287834908730'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+    const window = Dimensions.get("window");    
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return (<LoginForm />);
+      default:
+        return (
+          <View 
+            style={{ height: window.height }} >
+            <Spinner size="large" />
+          </View>
+        );
+    }
+  }
+
 
   render() {
     return (
       <View>
-      <Header headerText="Authentication"/>
-        <LoginForm />
+        <Header headerText="Authentication" />
+        <View>
+            {this.renderContent()}
+        </View>
+          
       </View>
     );
   }
